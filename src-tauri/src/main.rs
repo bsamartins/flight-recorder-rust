40a4e1d659ad9f5797f612dd47bd593aa87872be
Bernardo Martins <bsamartins@gmail.com>
2023-11-16 02:15:25 +0000

    Add simconnect dependency

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,80 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use simconnect_sdk::{Notification, SimConnect, SimConnectObject};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+// fn main() {
+//     tauri::Builder::default()
+//         .invoke_handler(tauri::generate_handler![greet])
+//         .run(tauri::generate_context!())
+//         .expect("error while running tauri application");
+// }
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = SimConnect::new("Receiving data example");
+
+    match client {
+        Ok(mut client) => {
+            let mut notifications_received = 0;
+
+            loop {
+                let notification = client.get_next_dispatch()?;
+
+                match notification {
+                    Some(Notification::Open) => {
+                        println!("Connection opened.");
+
+                        // After the connection is successfully open, we register the struct
+                        client.register_object::<AirplaneData>()?;
+                    }
+                    Some(Notification::Object(data)) => {
+                        if let Ok(airplane_data) = AirplaneData::try_from(&data) {
+                            println!("{airplane_data:?}");
+
+                            notifications_received += 1;
+
+                            // After we have received 10 notifications, we unregister the struct
+                            if notifications_received > 10 {
+                                client.unregister_object::<AirplaneData>()?;
+                                println!("Subscription stopped.");
+                                break;
+                            }
+                        }
+                    }
+                    _ => (),
+                }
+
+                // sleep for about a frame to reduce CPU usage
+                std::thread::sleep(std::time::Duration::from_millis(16));
+            }
+        }
+        Err(e) => {
+            println!("Error: {e:?}")
+        }
+    }
+    Ok(())
+}
+
+#[derive(Debug, Clone, SimConnectObject)]
+#[simconnect(period = "second")]
+#[allow(dead_code)]
+struct AirplaneData {
+    #[simconnect(name = "TITLE")]
+    title: String,
+    #[simconnect(name = "CATEGORY")]
+    category: String,
+    #[simconnect(name = "PLANE LATITUDE", unit = "degrees")]
+    lat: f64,
+    #[simconnect(name = "PLANE LONGITUDE", unit = "degrees")]
+    lon: f64,
+    #[simconnect(name = "PLANE ALTITUDE", unit = "feet")]
+    alt: f64,
+    #[simconnect(name = "SIM ON GROUND")]
+    sim_on_ground: bool,
 }
