6899d6e04e90d5c69d3e63f8ecfc0a1b29384264
Bernardo Martins <bsamartins@gmail.com>
2024-03-31 22:53:11 +0100

    Refactor modules

---
+++ src-tauri/src/database/connection.rs
@@ -0,0 +1,15 @@
+use sea_orm::Database;
+use sea_orm::DatabaseConnection;
+use sea_orm::DbErr;
+use migration::{Migrator, MigratorTrait};
+
+pub async fn initialize(database: &str) -> Result<DatabaseConnection, DbErr> {
+    println!("Initializing database: {}", database);
+    let db = Database::connect(format!("sqlite:{}?mode=rwc", database)).await?;
+    println!("Database initialized");
+
+    Migrator::up(&db, None).await?;
+    println!("Migration executed");
+
+    return Ok(db);
+}
