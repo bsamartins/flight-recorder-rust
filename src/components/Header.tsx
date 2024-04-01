3e91663decac3cc5e2b1b25b7d70380a95060a09
Bernardo Martins <bsamartins@gmail.com>
2024-04-01 18:55:52 +0100

    Remove sidebar hide button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,7 @@
 import * as React from 'react';
 import GlobalStyles from '@mui/joy/GlobalStyles';
-import IconButton from '@mui/joy/IconButton';
 import Sheet from '@mui/joy/Sheet';
-import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
-import { toggleSidebar } from '../utils.ts';
 import Typography from "@mui/joy/Typography";
-import {Box} from "@mui/joy";
 import ConnectingAirportsIcon from "@mui/icons-material/ConnectingAirportsRounded";
 
 export default function Header() {
@@ -31,14 +27,6 @@ export default function Header() {
                     },
                 }}
             />
-            <IconButton
-                onClick={() => toggleSidebar()}
-                variant="outlined"
-                color="neutral"
-                size="sm"
-            >
-                <MenuRoundedIcon />
-            </IconButton>
             <ConnectingAirportsIcon/>
             <Typography level="title-lg">Flight Recorder</Typography>
         </Sheet>
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,19 +1,13 @@
 import * as React from 'react';
 import GlobalStyles from '@mui/joy/GlobalStyles';
-import Avatar from '@mui/joy/Avatar';
 import Box from '@mui/joy/Box';
-import Divider from '@mui/joy/Divider';
-import IconButton from '@mui/joy/IconButton';
 import List from '@mui/joy/List';
 import ListItem from '@mui/joy/ListItem';
 import ListItemButton, {listItemButtonClasses} from '@mui/joy/ListItemButton';
-import ListItemContent from '@mui/joy/ListItemContent';
-import Typography from '@mui/joy/Typography';
 import Sheet from '@mui/joy/Sheet';
 import FlightIcon from '@mui/icons-material/FlightRounded';
 import SupportRoundedIcon from '@mui/icons-material/SupportRounded';
 import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
-import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
 
 export default function Sidebar() {
     return (
@@ -23,7 +17,7 @@ export default function Sidebar() {
                 height: '100dvh',
                 width: 'var(--Sidebar-width)',
                 top: 0,
-                p: 2,
+                p: 1,
                 flexShrink: 0,
                 display: 'flex',
                 flexDirection: 'column',
@@ -35,7 +29,7 @@ export default function Sidebar() {
             <GlobalStyles
                 styles={{
                     ':root': {
-                        '--Sidebar-width': '260px',
+                        '--Sidebar-width': '52px',
                     },
                 }}
             />
@@ -62,9 +56,6 @@ export default function Sidebar() {
                     <ListItem>
                         <ListItemButton selected>
                             <FlightIcon/>
-                            <ListItemContent>
-                                <Typography level="title-sm">Flights</Typography>
-                            </ListItemContent>
                         </ListItemButton>
                     </ListItem>
                 </List>
@@ -81,31 +72,14 @@ export default function Sidebar() {
                     <ListItem>
                         <ListItemButton>
                             <SupportRoundedIcon />
-                            Support
                         </ListItemButton>
                     </ListItem>
                     <ListItem>
                         <ListItemButton>
                             <SettingsRoundedIcon />
-                            Settings
                         </ListItemButton>
                     </ListItem>
                 </List>
-            </Box>
-            <Divider />
-            <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
-                <Avatar
-                    variant="outlined"
-                    size="sm"
-                    src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286"
-                />
-                <Box sx={{ minWidth: 0, flex: 1 }}>
-                    <Typography level="title-sm">Siriwat K.</Typography>
-                    <Typography level="body-xs">siriwatk@test.com</Typography>
-                </Box>
-                <IconButton size="sm" variant="plain" color="neutral">
-                    <LogoutRoundedIcon />
-                </IconButton>
             </Box>
         </Sheet>
     );
