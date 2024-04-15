9076f4afcefe2c84c51943880135f96f10ef544a
Bernardo Martins <bsamartins@gmail.com>
2024-04-15 01:26:33 +0100

    Create flight recorder stub

---
+++ src-tauri/src/flight_recorder.rs
@@ -0,0 +1,60 @@
+use std::fmt::{Debug, Formatter};
+use std::time::Duration;
+
+use tokio_cron_scheduler::{Job, JobScheduler, JobSchedulerError};
+use uuid::Uuid;
+
+pub struct FlightRecorder {
+    scheduler: JobScheduler,
+}
+
+impl FlightRecorder {
+    pub async fn new() -> Result<FlightRecorder, JobSchedulerError> {
+        let scheduler = JobScheduler::new().await?;
+        return Ok(
+            FlightRecorder {
+                scheduler,
+            }
+        )
+    }
+
+    pub async fn start(self) -> Result<Uuid, String> {
+        tracing::info!("Starting recorder");
+        let job_result = Job::new_repeated(Duration::from_secs(1), Self::execute);
+        let job = match job_result {
+            Ok(job) => {
+                job
+            }
+            Err(err) => {
+                return Err(format!("Failed to start flight recorder: {err}"));
+            }
+        };
+        let add_result = self.scheduler.add(job).await;
+        let uuid = match add_result {
+            Ok(uuid) => {
+                uuid
+            }
+            Err(err) => {
+                return Err(format!("Failed to start flight recorder: {err}"));
+            }
+        };
+        let scheduler_start_result = self.scheduler.start().await;
+        match scheduler_start_result {
+            Ok(_) => {}
+            Err(err) => {
+                return Err(format!("Failed to start flight recorder: {err}"));
+            }
+        }
+        Ok(uuid)
+    }
+
+    fn execute(uuid: Uuid, scheduler: JobScheduler) {
+        tracing::info!("Executing task {uuid}")
+    }
+}
+
+impl Debug for FlightRecorder {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "FlightRecorder{{}}")
+    }
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,26 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::any::Any;
 use std::error::Error;
+use std::fmt::Display;
 use std::sync::Arc;
 use std::thread::spawn;
 
 use futures::lock::Mutex;
 use sea_orm::DatabaseConnection;
 use tauri::{App, Manager};
-use tracing::{info, Level};
+use tokio_cron_scheduler::JobSchedulerError;
+use tracing::Level;
 use tracing_subscriber;
+use tracing_subscriber::EnvFilter;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 
 use flight_instrumentation::FlightInstrumentation;
 use state::FlightState;
+
+use crate::flight_recorder::FlightRecorder;
 
 mod cmd;
 mod database;
@@ -20,13 +28,19 @@ mod flight_instrumentation;
 mod model;
 mod repositories;
 mod state;
+mod flight_recorder;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let filter = EnvFilter::from_default_env()
+        .add_directive("sqlx::query=error".parse()?);
+
     tracing_subscriber::fmt()
+        .with_env_filter(filter)
         .with_max_level(Level::INFO)
-        .init();    
+        .init();
+
     let _ = tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .setup(setup)
@@ -42,21 +56,26 @@ async fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn setup(app: &mut App) -> Result<(), Box<dyn Error>> {
-    let db = initialize_database(app)?;
+    let db = setup_database(app)?;
 
-    spawn(|| {
-        let mut flight_instrumentation = FlightInstrumentation::new();
-        flight_instrumentation.set_listener(|data| {
-            tracing::info!("{data:?}")
-        });
-        let _ = flight_instrumentation
-            .start();
+    let _ = tauri::async_runtime::spawn(async {
+        let result = setup_instrumentation().await;
+        match result {
+            Ok(_) => {}
+            Err(err) => tracing::error!("Failed to initialize {}", err)
+        }
+
+        let result = setup_recorder().await;
+        match result {
+            Ok(_) => {}
+            Err(err) => tracing::error!("Failed to initialize {}", err)
+        }
     });
 
     Ok(())
 }
 
-fn initialize_database(app: &mut App) -> Result<Arc<DatabaseConnection>, Box<dyn Error>> {
+fn setup_database(app: &mut App) -> Result<Arc<DatabaseConnection>, Box<dyn Error>> {
     let database_path = app
         .path()
         .app_local_data_dir()
@@ -73,4 +92,25 @@ fn initialize_database(app: &mut App) -> Result<Arc<DatabaseConnection>, Box<dyn
         guard.clone().manage(db.clone());
         Ok(Arc::new(db.clone()))
     });
+}
+
+async fn setup_instrumentation() -> Result<(), String> {
+    let mut flight_instrumentation = FlightInstrumentation::new();
+    flight_instrumentation.set_listener(|data| {
+        tracing::info!("{data:?}")
+    });
+    let _ = flight_instrumentation
+        .start();
+
+    Ok(())
+}
+
+async fn setup_recorder() -> Result<(), String> {
+    let flight_recorder_res = FlightRecorder::new().await;
+    let flight_recorder = match flight_recorder_res {
+        Ok(fr) => { fr }
+        Err(err) => return Err(format!("Failed to initialise flight recorder {err}"))
+    };
+    let _ = flight_recorder.start().await;
+    Ok(())
 }
