3cd9e61cded6e10f8aaaae7bf636bd3a82241874
Bernardo Martins <bsamartins@gmail.com>
2026-01-26 21:37:26 +0000

    Fix lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import typescriptEslint from '@typescript-eslint/eslint-plugin';
 import react from 'eslint-plugin-react';
 import prettier from 'eslint-plugin-prettier';
@@ -12,46 +12,52 @@ import { FlatCompat } from '@eslint/eslintrc';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default defineConfig([{
+export default defineConfig([
+  {
     extends: compat.extends(
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended',
-        'prettier',
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'prettier',
     ),
 
     plugins: {
-        '@typescript-eslint': typescriptEslint,
-        react,
-        prettier,
+      '@typescript-eslint': typescriptEslint,
+      react,
+      prettier,
     },
 
     languageOptions: {
-        globals: {
-            ...globals.browser,
-        },
+      globals: {
+        ...globals.browser,
+      },
 
-        parser: tsParser,
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
 
     settings: {
-        'import/resolver': {
-            typescript: {},
-        },
+      'import/resolver': {
+        typescript: {},
+      },
+      react: {
+        version: 'detect',
+      },
     },
 
     rules: {
-        'react/react-in-jsx-scope': 'off',
-        camelcase: 'error',
-        'spaced-comment': 'error',
-        quotes: ['error', 'single'],
-        'no-duplicate-imports': 'error',
+      'react/react-in-jsx-scope': 'off',
+      camelcase: 'error',
+      'spaced-comment': 'error',
+      quotes: ['error', 'single'],
+      'no-duplicate-imports': 'error',
     },
-}]);
+  },
+  globalIgnores(['node_modules/', 'dist/', 'build/', 'src-tauri/', '.git/']),
+]);
